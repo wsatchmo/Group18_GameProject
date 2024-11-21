dda4739da195a819be8f33a0a064e6a86d0e03c0
bapped-cagd <baphan@csuchico.edu>
2024-11-21 11:40:44 -0800

    Camera and Crosshair

---
+++ Group18_Game/Assets/Scripts/CamMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+        //s mouseLook.x = Mathf.Clamp(mouseLook.x, -90f, 90f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+    }
+}
